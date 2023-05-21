1537bf9120cc133ce94029f145bcd273def05a85
lzielonka <lukziielonka@gmail.com>
2023-05-21 21:08:24 +0200

    tg relay

--- tg-to-discord-relay.js
+++ tg-to-discord-relay.js
@@ -1,19 +1,16 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const { WebhookClient } = require('discord.js');
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const TELEGRAM_CHANNEL_ID = parseInt(process.env.TELEGRAM_CHANNEL_ID, 10);
 
-const telegramBot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
-const webhook = new WebhookClient({ url: DISCORD_WEBHOOK_URL });
+const telegramBot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const webhook = new WebhookClient({ url: process.env.DISCORD_WEBHOOK_URL });
 
-telegramBot.on('channel_post', async (telegramMsg) => {
-    // const { chat, text, from } = telegramMsg;
-    const { chat, text } = telegramMsg;
-    if (chat.id === TELEGRAM_CHANNEL_ID) {
+telegramBot.on('message', async (msg) => {
+    const sender = msg.from.first_name || '?';
+    if (msg.chat.id === TELEGRAM_CHANNEL_ID) {
         try {
-            await webhook.send(text);
+            await webhook.send(sender + ': ' + msg.text);
         } catch (e) {}
     }
 });
